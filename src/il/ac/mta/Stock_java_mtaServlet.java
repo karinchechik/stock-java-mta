3b4e06fec89702a55d40edb336ea578beacdb716
karinchechik <karin.chechik@gmail.com>
2014-11-02 19:31:09 +0200

    added karin to the print

--- src/il/ac/mta/Stock_java_mtaServlet.java
+++ src/il/ac/mta/Stock_java_mtaServlet.java
@@ -8,6 +8,6 @@ public class Stock_java_mtaServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		resp.getWriter().println("Hello, karin chechik");
 	}
 }
