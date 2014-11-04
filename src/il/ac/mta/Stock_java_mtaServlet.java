640dfdb865713b26f12bf5238293e243c4937db5
karinchechik <karin.chechik@gmail.com>
2014-11-04 11:43:09 +0200

    Ex02 - insert calculation with 3 variables. 3*4*7=49

--- src/il/ac/mta/Stock_java_mtaServlet.java
+++ src/il/ac/mta/Stock_java_mtaServlet.java
@@ -7,7 +7,24 @@ import javax.servlet.http.*;
 public class Stock_java_mtaServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, karin chechik");
+		resp.setContentType("text/html");
+		resp.getWriter().println("Hello, karin chechik/n/n");
+		
+		// variables declaration:
+		int num1;
+		int num2;
+		int num3;
+		
+		// variable assignment:
+		num1 = 4;
+		num2 = 3;
+		num3 = 7;
+		
+		int result = num1 * num2 * num3;
+		
+		String resultStr = new String("<h1>Result of " + num1 + "*" + num2 + "*" + num3 + " = " + result + "</h1>");
+		
+		// add result to print line:
+		resp.getWriter().println(resultStr);
 	}
 }
