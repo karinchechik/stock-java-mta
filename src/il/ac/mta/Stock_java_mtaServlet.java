1a902b275a418551413afe43386f0792fa853460
karinchechik <karin.chechik@gmail.com>
2014-11-14 16:48:28 +0200

    add 3 calculations - ex 03. area of circlr, sin, radian, power.

--- src/il/ac/mta/Stock_java_mtaServlet.java
+++ src/il/ac/mta/Stock_java_mtaServlet.java
@@ -5,25 +5,43 @@ import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
 public class Stock_java_mtaServlet extends HttpServlet {
+	
+	public final double PIE = Math.PI;
+	
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		resp.setContentType("text/html");
 		resp.getWriter().println("Hello, karin chechik");
 		
 		// variables declaration:
-		int num1;
-		int num2;
-		int num3;
+		int radius;
+		double area;
+		int angleB;
+		int hypotenuse;
+		double opposite;
+		int base;
+		int exp;
+		double result;
+		double angleBDegrees;
 		
 		// variable assignment:
-		num1 = 4;
-		num2 = 3;
-		num3 = 7;
+		radius = 50;
+		angleB = 30;
+		hypotenuse = 50;
+		base = 20;
+		exp = 13;
+		angleBDegrees = Math.toRadians(angleB);
 		
-		int result = (num1 + num2) * num3;
+		area = radius * radius * PIE;
+		opposite = hypotenuse * Math.sin(angleBDegrees);
+		result = Math.pow(base, exp);
 		
-		String resultStr = new String("<h1>Result of ( " + num1 + "+" + num2 + " )*" + num3 + " = " + result + "</h1>");
-		
+		String line1 = new String("<h1>Calculation 1: Area of circle with radius " + radius + " is " + area + " square-cm." + "</h1>");
+		String line2 = new String("<h1>Calculation 2: Length of opposite where angle B is " + angleB + " degrees and Hypotenuse length is " + hypotenuse + " cm is: " + opposite + "." + "</h1>");
+		String line3 = new String("<h1>Calculation 3: Power of " + base + " with exp " + exp + " is: " + result + "</h1>");
+
+		String resultStr = line1 + "<br>" + line2 + "<br>" + line3;
+
 		// add result to print line:
 		resp.getWriter().println(resultStr);
 	}
